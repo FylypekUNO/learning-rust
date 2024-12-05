63fc1b6f9df08f29b8ebe116afbd1bff4896514e
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-12-05 17:31:23 +0100

    array/dict prints

---
+++ learn-print-v2/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+  let array = [1, 2, 3, 4, 5];
+
+  // println!("Array {{}}: {}", array); // cannot be formatted with the default formatter
+  println!("Array {{:?}}: {:?}", array);
+  println!("Array {{:#?}}: {:#?}", array);
+
+  let tuple = (1, 2, 3, 4, 5);
+
+  // println!("Tuple {{}}: {}", tuple); // cannot be formatted with the default formatter
+  println!("Tuple {{:?}}: {:?}", tuple);
+  println!("Tuple {{:#?}}: {:#?}", tuple);
+
+  let dict = vec![
+    ("key1", "value1"),
+    ("key2", "value2"),
+    ("key3", "value3"),
+  ];
+
+  // println!("Dict {{}}: {}", dict); // cannot be formatted with the default formatter
+  println!("Dict {{:?}}: {:?}", dict);
+  println!("Dict {{:#?}}: {:#?}", dict);
+}
