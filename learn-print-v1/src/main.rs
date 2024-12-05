79e991104bac8351539643e86c0dbd10f79e7cdd
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-12-05 17:27:08 +0100

    basic prints

--- learn-print-v1/src/main.rs
+++ learn-print-v1/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
   println!("Hello, world!");
+
+  let string = "Hello, world!";
+  println!("{} {:?}", string, string);
+
+  let number = 10;
+  println!("num: {}", number);
+
+  let boolean = true;
+  println!("bool: {} - {}", boolean, boolean as u8);
+
+  let float = 3.141592653589793;
+  println!("float: {} {:?} {:.4} {:e} {:.2e}", float, float, float, float, float);
+
+  let precision = 3;
+  println!("float with precision: {:.prec$}", float, prec = precision);
 }
